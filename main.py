9a4c942f174e386d8bd180c88cff93053b3369b8
José De Freitas <37962411+JoseDeFreitas@users.noreply.github.com>
2021-02-19 08:52:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import os
 
 def main():
-    if not os.path.isdir("generated"):
-        os.mkdir("generated")
-    with open("templates/overview.txt", "r") as f:
+    with open("file.txt", "r") as f:
         output = f.readlines()
     output[0] = 'testing'
-    with open("generated/overview.txt", "w") as f:
+    with open("file.txt", "w") as f:
         f.writelines(output)
 
 if __name__ == '__main__':
